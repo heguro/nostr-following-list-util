10045148aeff343d64f75244a2f530be2abc8ffc
heguro <65112898+heguro@users.noreply.github.com>
2023-02-17 21:56:22 +0900

    nip07 compatibility re-fix (type only)

--- src/@types/nip07.d.ts
+++ src/@types/nip07.d.ts
@@ -1,12 +1,15 @@
 import { NostrEvent } from './nostrTools';
 
 interface Nip07Nostr {
+  // must defined
   getPublicKey(): Promise<string>;
   signEvent(event: NostrEvent): Promise<NostrEvent>;
+
+  // optional
   getRelays?(): Promise<Nip07Relays>;
-  nip04: {
-    encrypt(pubkey: string, plaintext: string): Promise<string>;
-    decrypt(pubkey: string, ciphertext: string): Promise<string>;
+  nip04?: {
+    encrypt?(pubkey: string, plaintext: string): Promise<string>;
+    decrypt?(pubkey: string, ciphertext: string): Promise<string>;
   };
 }
 
